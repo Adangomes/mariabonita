2564dea23c80f4ccc18275bed69404f6fee10f24
Adan Gomes <adangones55@gmail.com>
2026-08-10 13:51:11 -0300

    Add initReservas function and global event listeners
    Initialize reservation form and set global functions for UI interactions.

--- reservas/index.js
+++ reservas/index.js
@@ -1,1 +1,28 @@
+// reservas/index.js
+import { setReservasData } from "./reservasState.js";
+import { renderReservas } from "./reservasUI.js";
+import { acaoImprimirReserva } from "./reservasPrint.js";
+import { 
+    criarReserva, 
+    adicionarItemReserva, 
+    removerItemReserva, 
+    concluirVendaReserva, 
+    cancelarReserva 
+} from "./reservasActions.js";
 
+export { setReservasData };
+
+export function initReservas() {
+    const formReserva = document.getElementById('formReserva');
+    if (formReserva) {
+        formReserva.addEventListener('submit', criarReserva);
+    }
+
+    // Exposição global para chamadas inline do HTML (onclick)
+    window.renderReservas = renderReservas;
+    window.acaoImprimirReserva = acaoImprimirReserva;
+    window.adicionarItemReserva = adicionarItemReserva;
+    window.removerItemReserva = removerItemReserva;
+    window.concluirVendaReserva = concluirVendaReserva;
+    window.cancelarReserva = cancelarReserva;
+}
