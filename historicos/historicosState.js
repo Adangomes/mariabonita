952f244199e32647a404062d1a7099bd9327a88d
Adan Gomes <adangones55@gmail.com>
2026-08-10 13:31:39 -0300

    Add functions to set and get vendas data

--- historicos/historicosState.js
+++ historicos/historicosState.js
@@ -1,1 +1,11 @@
+// historicos/historicosState.js
 
+let vendasLocal = [];
+
+export function setVendasData(vendas) {
+    vendasLocal = vendas;
+}
+
+export function getVendasData() {
+    return vendasLocal;
+}
