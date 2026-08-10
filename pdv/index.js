a6614d53851fddf381d0cec1041dcddb6fe043b8
Adan Gomes <adangones55@gmail.com>
2026-08-10 13:56:33 -0300

    Implement PDV initialization and event binding
    Initialize PDV by binding events and exposing functions to global window object.

--- pdv/index.js
+++ pdv/index.js
@@ -1,1 +1,25 @@
+// pdv/index.js
+import { setPdvData } from "./pdvState.js";
+import { renderCarrinho } from "./pdvUI.js";
+import { 
+    buscarEAdicionar, 
+    alterarQtdCarrinho, 
+    removerDoCarrinho, 
+    limparCarrinho, 
+    finalizarVenda 
+} from "./pdvActions.js";
+import { bindPdvEvents } from "./pdvEvents.js";
 
+export { setPdvData };
+
+export function initPdv() {
+    bindPdvEvents();
+
+    // Expõe as funções para a janela global (necessário para os onclicks no HTML)
+    window.buscarEAdicionar = buscarEAdicionar;
+    window.renderCarrinho = renderCarrinho;
+    window.alterarQtdCarrinho = alterarQtdCarrinho;
+    window.removerDoCarrinho = removerDoCarrinho;
+    window.limparCarrinho = limparCarrinho;
+    window.finalizarVenda = finalizarVenda;
+}
