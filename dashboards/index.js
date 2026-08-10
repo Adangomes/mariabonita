b3bc4c0e0e6c6450ee9ac86adde5c25aa14e5af9
Adan Gomes <adangones55@gmail.com>
2026-08-10 13:10:11 -0300

    Implement updateDashboard function for dashboard updates

--- dashboards/index.js
+++ dashboards/index.js
@@ -1,1 +1,21 @@
+// dashboards/index.js
+import { calcularMetricas, atualizarCardsUI } from "./dashboardMetrics.js";
+import { agruparPagamentos, agruparOrigemReservas } from "./dashboardData.js";
+import { renderizarGraficoPagamentos, renderizarGraficoOrigem } from "./dashboardCharts.js";
 
+export function updateDashboard(vendas = [], reservas = []) {
+    // 1. Atualiza Indicadores/Cards
+    const metricas = calcularMetricas(vendas, reservas);
+    atualizarCardsUI(metricas);
+
+    // 2. Agrupa os Dados
+    const pagMap = agruparPagamentos(vendas);
+    const origemMap = agruparOrigemReservas(reservas);
+
+    // 3. Renderiza os Gráficos
+    renderizarGraficoPagamentos(pagMap);
+    renderizarGraficoOrigem(origemMap);
+}
+
+// Expõe globalmente caso o sistema chame via window
+window.updateDashboard = updateDashboard;
