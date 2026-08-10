862ec4abfbc4d9985841e5d762b2a8b712571082
Adan Gomes <adangones55@gmail.com>
2026-08-10 13:32:39 -0300

    Initialize historico functions for global access
    Expose renderHistorico, limparFiltroData, and gerarPDFHistorico functions globally for HTML event calls.

--- historicos/index.js
+++ historicos/index.js
@@ -1,1 +1,13 @@
+// historicos/index.js
+import { setVendasData } from "./historicosState.js";
+import { renderHistorico, limparFiltroData } from "./historicosUI.js";
+import { gerarPDFHistorico } from "./historicosPdf.js";
 
+export { setVendasData };
+
+export function initHistorico() {
+    // Exposição global para chamadas via eventos no HTML (ex: onclick)
+    window.renderHistorico = renderHistorico;
+    window.limparFiltroData = limparFiltroData;
+    window.gerarPDFHistorico = gerarPDFHistorico;
+}
