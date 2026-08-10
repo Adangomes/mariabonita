b68b5492792cd39ad0902ba8886f4534c1e53cc2
Adan Gomes <adangones55@gmail.com>
2026-08-10 13:54:51 -0300

    Add functions to manage PDV state and data

--- pdv/pdvState.js
+++ pdv/pdvState.js
@@ -1,1 +1,26 @@
+// pdv/pdvState.js
 
+let carrinho = [];
+let produtosLocal = [];
+let vendasLocal = [];
+
+export function setPdvData(prod, vend) {
+    produtosLocal = prod || [];
+    vendasLocal = vend || [];
+}
+
+export function getCarrinho() {
+    return carrinho;
+}
+
+export function setCarrinho(novoCarrinho) {
+    carrinho = novoCarrinho;
+}
+
+export function getProdutosData() {
+    return produtosLocal;
+}
+
+export function getVendasData() {
+    return vendasLocal;
+}
