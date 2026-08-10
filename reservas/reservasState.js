c6416cc1e992a9ffa717079fadb6052dc8d9e963
Adan Gomes <adangones55@gmail.com>
2026-08-10 13:49:42 -0300

    Update reservasState.js

--- reservas/reservasState.js
+++ reservas/reservasState.js
@@ -1,1 +1,17 @@
+// reservas/reservasState.js
 
+let reservasLocal = [];
+let produtosLocal = [];
+
+export function setReservasData(res, prod) {
+    reservasLocal = res || [];
+    produtosLocal = prod || [];
+}
+
+export function getReservasData() {
+    return reservasLocal;
+}
+
+export function getProdutosData() {
+    return produtosLocal;
+}
